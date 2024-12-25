f03c4ddef3c954f7a7701e713e9894e8f8dfc8b9
kobi075@gmail.com <kobi075@gmail.com>
2024-12-25 04:46:36 +0200

    first commit

---
+++ Assets/Scripts/0-bfs/BFS.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+/**
+ * A generic implementation of the BFS algorithm.
+ * @author Erel Segal-Halevi
+ * @since 2020-02
+ */
+public class BFS {
+
+    public static List<Vector3Int> GetReachableTiles(TilemapGraph tilemapGraph, Vector3Int startPosition, int maxIterations = 1000)
+    {
+        List<Vector3Int> reachableTiles = new List<Vector3Int>();
+
+        Queue<Vector3Int> openQueue = new Queue<Vector3Int>();
+        HashSet<Vector3Int> visited = new HashSet<Vector3Int>();
+
+        openQueue.Enqueue(startPosition);
+        visited.Add(startPosition);
+
+        int iterations = 0;
+
+        while (openQueue.Count > 0 && iterations < maxIterations)
+        {
+            iterations++;
+            Vector3Int currentTile = openQueue.Dequeue();
+            reachableTiles.Add(currentTile);
+
+            foreach (var neighbor in tilemapGraph.Neighbors(currentTile))
+            {
+                if (!visited.Contains(neighbor))
+                {
+                    openQueue.Enqueue(neighbor);
+                    visited.Add(neighbor);
+                }
+            }
+        }
+
+        return reachableTiles;
+    }
+
+    // This is the existing GetPath method in the BFS class
+    public static List<NodeType> GetPath<NodeType>(IGraph<NodeType> graph, NodeType startNode, NodeType endNode, int maxiterations = 1000)
+    {
+        List<NodeType> path = new List<NodeType>();
+        FindPath(graph, startNode, endNode, path, maxiterations);
+        return path;
+    }
+
+
+
+    public static void FindPath<NodeType>(
+            IGraph<NodeType> graph, 
+            NodeType startNode, NodeType endNode, 
+            List<NodeType> outputPath, int maxiterations=1000)
+    {
+        Queue<NodeType> openQueue = new Queue<NodeType>();
+        HashSet<NodeType> openSet = new HashSet<NodeType>();
+        Dictionary<NodeType, NodeType> previous = new Dictionary<NodeType, NodeType>();
+        openQueue.Enqueue(startNode);
+        openSet.Add(startNode);
+        int i; for (i = 0; i < maxiterations; ++i) { // After maxiterations, stop and return an empty path
+            if (openQueue.Count == 0) {
+                break;
+            } else {
+                NodeType searchFocus = openQueue.Dequeue();
+
+                if (searchFocus.Equals(endNode)) {
+                    // We found the target -- now construct the path:
+                    outputPath.Add(endNode);
+                    while (previous.ContainsKey(searchFocus)) {
+                        searchFocus = previous[searchFocus];
+                        outputPath.Add(searchFocus);
+                    }
+                    outputPath.Reverse();
+                    break;
+                } else {
+                    // We did not found the target yet -- develop new nodes.
+                    foreach (var neighbor in graph.Neighbors(searchFocus)) {
+                        if (openSet.Contains(neighbor)) {
+                            continue;
+                        }
+                        openQueue.Enqueue(neighbor);
+                        openSet.Add(neighbor);
+                        previous[neighbor] = searchFocus;
+                    }
+                }
+            }
+        }
+    }
+
+    public static List<NodeType> GetPath<NodeType>(TilemapGraph tilemapGraph, IGraph<NodeType> graph, NodeType startNode, NodeType endNode, int maxiterations=1000) {
+        List<NodeType> path = new List<NodeType>();
+        FindPath(graph, startNode, endNode, path, maxiterations);
+        return path;
+    }
+
+}
---
+++ Assets/Scripts/2-player/KeyboardMoverByTile.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class KeyboardMoverByTile : KeyboardMover
+{
+    [SerializeField] Tilemap tilemap = null;
+    [SerializeField] AllowedTiles allowedTiles = null;
+    [Tooltip("Maximum number of iterations before BFS algorithm gives up on finding a path")]
+    [SerializeField] int maxIterations = 1000;
+
+    bool playerPlaced = false;
+    bool playerPositionValidated = false;
+
+    private TilemapGraph tilemapGraph = null;
+
+    void Start()
+    {
+        tilemapGraph = new TilemapGraph(tilemap, allowedTiles.Get());
+    }
+
+    void Update()
+    {
+        // Check if the player has been placed on a tile, if not, place the player
+        if (!playerPlaced)
+        {
+            PlacePlayerOnRandomAllowedTile();
+            playerPlaced = true;
+        }
+
+        // Validate player position once they are placed
+        if (playerPlaced && !playerPositionValidated)
+        {
+            ValidatePlayerPosition();
+            playerPositionValidated = true;
+        }
+
+        Vector3 newPosition = NewPosition();
+        TileBase tileOnNewPosition = TileOnPosition(newPosition);
+        if (allowedTiles.Contains(tileOnNewPosition))
+        {
+            transform.position = newPosition;
+        }
+        else
+        {
+            Debug.Log("You cannot walk on " + tileOnNewPosition + "!");
+        }
+    }
+
+    private void PlacePlayerOnRandomAllowedTile()
+    {
+        // Find all positions of allowed tiles
+        List<Vector3Int> allowedTilePositions = new List<Vector3Int>();
+        BoundsInt bounds = tilemap.cellBounds;
+        foreach (var position in bounds.allPositionsWithin)
+        {
+            if (allowedTiles.Contains(tilemap.GetTile(position)))
+            {
+                allowedTilePositions.Add(position);
+            }
+        }
+
+        // Select a random position from the allowed tile positions
+        Vector3Int randomPosition = allowedTilePositions[Random.Range(0, allowedTilePositions.Count)];
+        // Convert the position to world coordinates
+        Vector3 worldPosition = tilemap.CellToWorld(randomPosition);
+        // Set the player's position to the random position
+        transform.position = worldPosition;
+    }
+
+    private void ValidatePlayerPosition()
+    {
+        Vector3Int playerPosition = tilemap.WorldToCell(transform.position);
+        List<Vector3Int> reachableTiles = BFS.GetReachableTiles(tilemapGraph, playerPosition, maxIterations);
+
+        if (reachableTiles.Count < 100)
+        {
+            // Player is not in a valid position, reposition them
+            Debug.LogWarning("Player is in a position with less than 100 reachable tiles. Repositioning...");
+            PlacePlayerOnRandomAllowedTile();
+            ValidatePlayerPosition(); // Validate again recursively
+        }
+        else
+        {
+            Debug.Log("Player positioned in a valid location.");
+        }
+    }
+
+    private TileBase TileOnPosition(Vector3 worldPosition)
+    {
+        Vector3Int cellPosition = tilemap.WorldToCell(worldPosition);
+        return tilemap.GetTile(cellPosition);
+    }
+}
